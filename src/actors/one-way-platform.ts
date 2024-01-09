444c3f286cf18f57a7b7150131f0a4ec8d40cf15
Matt Jennings <me@mattjennings.io>
2024-01-08 23:29:31 -0600

    simpler one-way platforms

--- src/actors/one-way-platform.ts
+++ src/actors/one-way-platform.ts
@@ -4,27 +4,20 @@ export class OneWayPlatform extends ex.Actor {
   constructor(args: ex.ActorArgs) {
     super({
       ...args,
-      collisionType: ex.CollisionType.Passive,
+      collisionType: ex.CollisionType.Fixed,
       anchor: ex.vec(0, 0),
       height: 4,
     })
-
-    this.on('collisionstart', this.onCollisionStart.bind(this))
-    this.on('collisionend', this.onCollisionEnd.bind(this))
   }
 
-  /**
-   * When an actor collides with the platform, if the actor is falling, then
-   * the platform becomes solid. Otherwise, the platform is passable.
-   */
-  onCollisionStart(ev: ex.CollisionStartEvent) {
-    const other = ev.other
-    if (other instanceof ex.Actor && other.vel.y >= 0) {
-      this.body.collisionType = ex.CollisionType.Fixed
+  onPreCollisionResolve(
+    self: ex.Collider,
+    other: ex.Collider,
+    side: ex.Side,
+    contact: ex.CollisionContact
+  ): void {
+    if (other.owner instanceof ex.Actor && other.owner.vel.y < 0) {
+      contact.cancel()
     }
-  }
-
-  onCollisionEnd() {
-    this.body.collisionType = ex.CollisionType.Passive
   }
 }
--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -1,3 +1,4 @@
+import * as ex from 'excalibur'
 import { Resources } from '../resources'
 import BaseLevelScene from '../classes/base-level-scene'
 import { TiledResources } from '../resources.tiled'
@@ -8,5 +9,16 @@ export default class Level1 extends BaseLevelScene {
       tilemap: TiledResources.tilemap_level1,
       background: Resources.img_level1_bg,
     })
+
+    // add 16x16 physics squar
+    const square = new ex.Actor({
+      x: 80,
+      y: 100,
+      width: 16,
+      height: 16,
+      color: ex.Color.Red,
+      collisionType: ex.CollisionType.Active,
+    })
+    this.add(square)
   }
 }
