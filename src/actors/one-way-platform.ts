e0a5a91cfe65ebf3acf129a1b3800500e0da5d08
Matt Jennings <me@mattjennings.io>
2024-01-09 22:07:08 -0600

    turn one way collision logic into a component

--- src/actors/one-way-platform.ts
+++ src/actors/one-way-platform.ts
@@ -1,4 +1,5 @@
 import * as ex from 'excalibur'
+import { OneWayCollisionComponent } from '../components/one-way-collision'
 
 export class OneWayPlatform extends ex.Actor {
   constructor(args: ex.ActorArgs) {
@@ -8,28 +9,7 @@ export class OneWayPlatform extends ex.Actor {
       anchor: ex.vec(0, 0),
       height: 16,
     })
-  }
 
-  onPreCollisionResolve(
-    self: ex.Collider,
-    other: ex.Collider,
-    side: ex.Side,
-    contact: ex.CollisionContact
-  ): void {
-    if (!(other.owner instanceof ex.Actor)) return
-
-    // the difference between the current and previous position of the other actor
-    const otherPosDelta = other.owner.pos.sub(other.owner.oldPos)
-
-    // was the other actor above the platform in the previous frame?
-    const otherWasAbovePlatform =
-      other.bounds.bottom - otherPosDelta.y < self.bounds.top + 1
-
-    // ignore collision if the collision side is not on the top,
-    // or if other was not above the platform in the previous frame
-    if (side !== ex.Side.Top || !otherWasAbovePlatform) {
-      contact.cancel()
-      return
-    }
+    this.addComponent(new OneWayCollisionComponent())
   }
 }
