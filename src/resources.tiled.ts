9d1fd699321d43ed8e1235dd78e841262a6facc1
Matt Jennings <me@mattjennings.io>
2024-01-13 00:43:53 -0600

    respawn enemies

--- src/resources.tiled.ts
+++ src/resources.tiled.ts
@@ -6,6 +6,7 @@ import { TiledResource } from '@excaliburjs/plugin-tiled'
 import Player from './actors/player'
 import { OneWayPlatform } from './actors/platforms/one-way-platform'
 import { BugEnemy } from './actors/enemies/bug'
+import { EnemySpawner } from './actors/enemy-spawner'
 
 export const TiledResources = {
   tilemap_level1: new TiledResource('/res/tilemaps/level1.tmx', {
@@ -19,13 +20,17 @@ export const TiledResources = {
         }),
 
       BugEnemy: (props) =>
-        new BugEnemy({
+        new EnemySpawner({
           x: props.object?.x ?? 0,
           y: props.object?.y ?? 0,
-          z: props.layer.order ?? 0,
-          type:
-            (props.object?.properties.get('type') as 'green' | 'gray') ??
-            'green',
+          spawn: (pos) =>
+            new BugEnemy({
+              pos,
+              z: props.layer.order ?? 0,
+              type:
+                (props.object?.properties.get('type') as 'green' | 'gray') ??
+                'green',
+            }),
         }),
     },
   }),
